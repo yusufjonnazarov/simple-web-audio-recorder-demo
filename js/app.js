a00926fa362b83225c4cf7aa9e961f25cb5e195b
Yusuf <52450380+yusufjonnazarov@users.noreply.github.com>
2020-03-28 20:33:38 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function startRecording() {
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
     
-    var constraints = { audio: true, video:false }
+    var constraints = { audio: true, video:true }
 
     /*
     	We're using the standard promise based getUserMedia() 
